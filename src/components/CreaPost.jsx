f8b1e900877267c9a7d4cfa839b063c3f3ebbdc7
Antonio Lieto <antonio.lieto1999@gmail.com>
2025-06-05 10:12:10 +0200

    Update CreaPost.jsx

--- src/components/CreaPost.jsx
+++ src/components/CreaPost.jsx
@@ -1,20 +1,31 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
-import { Card, Form, Button, Row, Col } from "react-bootstrap";
+import { Card, Form, Button, Row, Col, Image } from "react-bootstrap";
+import { useSelector } from "react-redux";
 
 function CreatePost() {
+  const profileImg = useSelector((state) => state.profile.user.image);
+
   return (
     <Card className="mb-3 shadow-sm">
       <Card.Body>
-        <Form.Control
-          as="textarea"
-          placeholder="Crea un post"
-          rows={2}
-          className="rounded-pill px-3 py-2"
-          style={{ resize: "none" }}
-        />
+        <Row className="align-items-start mb-3">
+          <Col xs="auto">
+            <Image src={profileImg} roundedCircle width={65} height={65} alt="Profilo" />
+          </Col>
 
-        <Row className="mt-3 text-center me-3">
+          <Col>
+            <Form.Control
+              as="textarea"
+              placeholder="Crea un post"
+              rows={2}
+              className="rounded-pill px-3 py-2"
+              style={{ resize: "none" }}
+            />
+          </Col>
+        </Row>
+
+        <Row className="text-center me-3">
           <Col>
             <Button variant="light">
               <i className="bi bi-camera-video me-2" style={{ color: "green" }}></i>
@@ -29,7 +40,7 @@ function CreatePost() {
           </Col>
           <Col>
             <Button variant="light">
-              <i class="bi bi-newspaper me-2" style={{ color: "orange" }}></i>
+              <i className="bi bi-newspaper me-2" style={{ color: "orange" }}></i>
               Scrivi un articolo
             </Button>
           </Col>
